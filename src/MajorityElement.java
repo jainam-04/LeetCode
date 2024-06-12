a2facbf57b03a8cbadcd42b36272233f8b93f3d8
Jainam <jainamrupani04@gmail.com>
2024-06-12 10:37:02 +0530

    Majority Element

---
+++ src/MajorityElement.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class MajorityElement {
+    public static int majorityElement(int[] nums){
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 3};
+        System.out.println(majorityElement(nums));
+    }
+}
