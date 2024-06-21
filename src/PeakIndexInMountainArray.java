0de57744543bfd96ab2c4e57ce0348ddd2c64aa5
Jainam <jainamrupani04@gmail.com>
2024-06-21 16:14:53 +0530

    Peak Index In Mountain Array

---
+++ src/PeakIndexInMountainArray.java
@@ -0,0 +1,21 @@
+public class PeakIndexInMountainArray {
+    public static int peakIndexInMountainArray(int[] nums){
+        int start = 0;
+        int end = nums.length - 1;
+        while(start < end){
+            int mid = start + (end - start) / 2;
+            if(nums[mid] > nums[mid + 1]){
+                end = mid;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 5, 6, 4, 3, 2};
+        System.out.println(peakIndexInMountainArray(nums));
+    }
+}
