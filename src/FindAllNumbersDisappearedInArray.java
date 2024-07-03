a2a804b77212bb1a8eadea613a8d3213a83dfbd2
Jainam <jainamrupani04@gmail.com>
2024-07-03 10:18:00 +0530

    Find All Disappeared Numbers In An Array

---
+++ src/FindAllNumbersDisappearedInArray.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindAllNumbersDisappearedInArray {
+    public static List<Integer> findDisappearedNumbers(int[] nums){
+        int i = 0;
+        List<Integer> list = new ArrayList<>();
+        while(i < nums.length){
+            int correctIndex = nums[i] - 1;
+            if(nums[i] != nums[correctIndex]){
+                swap(nums, i, correctIndex);
+            }
+            else{
+                i++;
+            }
+        }
+        for(int index = 0; index < nums.length; index++){
+            if(nums[index] != index + 1){
+                list.add(index + 1);
+            }
+        }
+        return list;
+    }
+
+    public static void swap(int[] nums, int first, int second){
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
+        System.out.println(findDisappearedNumbers(nums));
+    }
+}
