d1463727b23d71ccd9a1efd818dcb64515bd3d98
Jainam <jainamrupani04@gmail.com>
2024-07-03 22:41:15 +0530

    Modified Find All Duplicate Numbers

--- src/FindAllDuplicateNumbers.java
+++ src/FindAllDuplicateNumbers.java
@@ -3,7 +3,7 @@ public class FindAllDuplicateNumbers {
         int i = 0;
         while(i < nums.length){
             int correctIndex = nums[i] - 1;
-            if((nums[i] < nums.length) && (nums[i] != nums[correctIndex])){
+            if(nums[i] != nums[correctIndex]){
                 swap(nums, i, correctIndex);
             }
             else{
