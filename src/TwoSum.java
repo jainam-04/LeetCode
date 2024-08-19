12cf0255ca12768944068d23a48153d435aa72e6
Jainam <jainamrupani04@gmail.com>
2024-08-19 22:50:21 +0530

    Update the solution of two sum

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,23 +1,16 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class TwoSum {
     public static int[] twoSum(int[] nums, int target){
-        if(nums.length == 2){
-            if(nums[0] + nums[1] == target){
-                return new int[]{0, 1};
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int current;
+        for(int i = 0; i < nums.length; i++){
+            current = target - nums[i];
+            if(map.containsKey(current)){
+                return new int[]{map.get(current), i};
             }
-            else{
-                return new int[]{-1, -1};
-            }
-        }
-        else{
-            for(int i = 0; i < nums.length; i++){
-                for(int j = i + 1; j < nums.length; j++){
-                    if(nums[i] + nums[j] == target){
-                        return new int[]{i, j};
-                    }
-                }
-            }
+            map.put(nums[i], i);
         }
         return new int[]{-1, -1};
     }
