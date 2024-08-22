f312de6d9afe18e7fc0a8b8cb4e3008dfc4f763c
Jainam <jainamrupani04@gmail.com>
2024-08-22 15:33:18 +0530

    Modified Power

--- src/Power.java
+++ src/Power.java
@@ -1,11 +1,26 @@
 public class Power {
     public static double pow(double x, int n){
-        return Math.pow(x, n);
+        return binaryExp(x, (long) n);
+    }
+
+    public static double binaryExp(double x, long n){
+        if(n == 0){
+            return 1;
+        }
+        if(n < 0){
+            return 1.0 / binaryExp(x, -n);
+        }
+        if(n % 2 == 1){
+            return x * binaryExp(x * x, (n - 1) / 2);
+        }
+        else{
+            return binaryExp(x * x, n / 2);
+        }
     }
 
     public static void main(String[] args) {
         double x = 2;
-        int n = 10;
+        int n = -2;
         System.out.println(pow(x, n));
     }
 }
