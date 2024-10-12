2d303c39c6d0787462343b2f5a4917c3f142e5a1
Jainam <jainamrupani04@gmail.com>
2024-10-12 20:43:23 +0530

    Number Of Steps To reduce A Number To Zero

---
+++ src/CountNumberOfSteps.java
@@ -0,0 +1,20 @@
+public class CountNumberOfSteps {
+    public static void main(String[] args) {
+        int num = 14;
+        System.out.println(numberOfSteps(num));
+    }
+
+    public static int numberOfSteps(int num){
+        return helper(num, 0);
+    }
+
+    public static int helper(int num, int steps){
+        if(num == 0){
+            return steps;
+        }
+        if(num % 2 == 0){
+            return helper(num / 2, steps + 1);
+        }
+        return helper(num - 1, steps + 1);
+    }
+}
