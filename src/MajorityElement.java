55c81d16df12f1fba0e77e95e355872988e899e1
Jainam <jainamrupani04@gmail.com>
2025-02-10 19:23:32 +0530

    Modified Majority Element

--- src/MajorityElement.java
+++ src/MajorityElement.java
@@ -1,13 +1,30 @@
-import java.util.Arrays;
-
 public class MajorityElement {
     public static int majorityElement(int[] nums){
-        Arrays.sort(nums);
-        return nums[nums.length / 2];
+        int majority_index = 0;
+        int count = 1;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[majority_index] == nums[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
+            if(count == 0){
+                majority_index = i;
+                count = 1;
+            }
+        }
+        count = 0;
+        for (int num : nums) {
+            if (nums[majority_index] == num) {
+                count++;
+            }
+        }
+        return nums[majority_index];
     }
 
     public static void main(String[] args) {
-        int[] nums = {3, 2, 3};
+        int[] nums = {8, 5, 8, 3, 8, 8, 8};
         System.out.println(majorityElement(nums));
     }
 }
