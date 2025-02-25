583a4c74c2666a12db0f6668b429b29c4d71ec83
Jainam <jainamrupani04@gmail.com>
2025-02-25 13:17:16 +0530

    Delete Head Node Of Circular Linked List

---
+++ src/deleteheadnodeofcircularlinkedlist/DeleteHeadNodeOfCircularLinkedList.java
@@ -0,0 +1,65 @@
+package deleteheadnodeofcircularlinkedlist;
+
+class ListNode{
+    int data;
+    ListNode next;
+
+    ListNode head = null;
+
+    public void push(int data){
+        ListNode node = new ListNode();
+        node.data = data;
+        if(head == null){
+            head = node;
+        }
+        else{
+            ListNode temp = head;
+            while(temp.next != head){
+                temp = temp.next;
+            }
+            temp.next = node;
+        }
+        node.next = head;
+    }
+
+    public void display(){
+        ListNode temp = head;
+        while(temp.next != head){
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println(temp.data);
+    }
+
+    public ListNode deleteHead(){
+        if(head == null || head.next == head){
+            head = null;
+            return null;
+        }
+        else{
+            ListNode temp = head;
+            while(temp.next != head){
+                temp = temp.next;
+            }
+            temp.next = head.next;
+            head = head.next;
+        }
+        return head;
+    }
+}
+
+public class DeleteHeadNodeOfCircularLinkedList {
+    public static void main(String[] args) {
+        ListNode node = new ListNode();
+        node.push(1);
+        node.push(2);
+        node.push(3);
+        node.push(4);
+        node.push(5);
+        System.out.print("Linked list before deleting the head node :- ");
+        node.display();
+        node.head = node.deleteHead();
+        System.out.print("Linked list after deleting the head node :- ");
+        node.display();
+    }
+}
