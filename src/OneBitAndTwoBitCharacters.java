25d9fb9b4e61219735bbed760f32740661360102
Jainam Rupani <jainamrupani04@gmail.com>
2025-11-18 15:06:38 +0530

    1-Bit And 2-Bit Characters

---
+++ src/OneBitAndTwoBitCharacters.java
@@ -0,0 +1,16 @@
+public class OneBitAndTwoBitCharacters {
+    public static void main(String[] args) {
+        int[] bits = {1, 1, 1, 0};
+        boolean result = isOneBitCharacter(bits);
+        System.out.println(result);
+    }
+
+    public static boolean isOneBitCharacter(int[] bits){
+        int n = bits.length;
+        int i = 0;
+        while(i < n - 1){
+            i += bits[i] + 1;
+        }
+        return i == n - 1;
+    }
+}
